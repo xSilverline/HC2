854891188cac3738f6fe86f4d890337472042049
xSilverline <32816233+xSilverline@users.noreply.github.com>
2018-03-01 20:31:49 +0100

    commit test

---
+++ src/main/java/HashCode/Solv2.java
@@ -0,0 +1,20 @@
+package HashCode;
+
+import java.util.ArrayList;
+
+public class Solv2
+{
+    private int yDist;
+    private int xDist;
+
+    private ArrayList<Car> cars = new ArrayList<>();
+    private ArrayList<Ride> rides = new ArrayList<>();
+
+    Solv2(ArrayList<Car> cars, ArrayList<Ride> rides)
+    {
+        this.cars=cars;
+        this.rides=rides;
+    }
+
+
+}
